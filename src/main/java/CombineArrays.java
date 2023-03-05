1536eb40dcb1a2001d3abd9f939e80cd5b895650
Domanrovil Ovalle <domanrovil.ovalle@gmail.com>
2023-03-05 02:29:08 +0000

    Completed Lab Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,16 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int[] combinedArray = new int[arr1.length + arr2.length];
+        int i =0;
+        for(int num: arr1){
+            combinedArray[i] = num;
+            i++;
+        }
+        for (int num: arr2){
+            combinedArray[i] = num;
+            i++;
+        }
+        return combinedArray;
     }
 }
